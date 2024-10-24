7ac293b0c91f4f4eda3b5320e80728d12d96aeae
Natalia Salatiel <nsalatimel91@gmail.com>
2024-10-24 19:45:18 -0300

    app finalizado

--- database/aplicativopythonflask.sql
+++ database/aplicativopythonflask.sql
@@ -1,19 +1,22 @@
+-- Remove o banco de dados se ele já existir
 DROP DATABASE IF EXISTS aplicativopythonflask;
 
+-- Cria um novo banco de dados com codificação UTF-8
 CREATE DATABASE aplicativopythonflask
-	character set utf8mb4
-    collate utf8mb4_general_ci;
-    
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+
+-- Seleciona o banco de dados recém-criado para uso
 USE aplicativopythonflask;
 
+-- Cria a tabela de tarefas com as colunas especificadas
 CREATE TABLE tasks (
-	task_id INT PRIMARY KEY AUTO_INCREMENT,
-    task_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    task_title VARCHAR(127) NOT NULL,
-    task_description TEXT NOT NULL,
-    task_status ENUM('concluido', 'pendente', 'del') DEFAULT 'pendente'
+    task_id INT PRIMARY KEY AUTO_INCREMENT, -- Identificador único da tarefa, auto-incrementado
+    task_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data e hora da criação da tarefa, padrão para o momento atual
+    task_title VARCHAR(127) NOT NULL, -- Título da tarefa, não pode ser nulo
+    task_description TEXT NOT NULL, -- Descrição da tarefa, não pode ser nula
+    task_status ENUM('concluido', 'pendente', 'del') DEFAULT 'pendente' -- Status da tarefa, padrão é 'pendente'
 );
 
- 
-
+-- Seleciona todas as tarefas da tabela (não retornará resultados, pois a tabela está vazia após a criação)
 SELECT * FROM tasks;
--- index.py
+++ index.py
@@ -5,95 +5,88 @@ from datetime import datetime
 # Cria uma instância da aplicação Flask
 app = Flask(__name__)
 
-# Dados de configuração para a conexão com o banco de dados MySQL
+# Configurações do banco de dados MySQL
 app.config['MYSQL_HOST'] = 'localhost'  # Endereço do servidor MySQL
 app.config['MYSQL_USER'] = 'root'       # Nome de usuário do MySQL
 app.config['MYSQL_PASSWORD'] = ''       # Senha do usuário do MySQL (vazia neste caso)
 app.config['MYSQL_DB'] = 'aplicativopythonflask'  # Nome do banco de dados a ser utilizado
 
-# Cria uma conexão com o MySQL usando as configurações acima
+# Inicializa a conexão com o MySQL
 mysql = MySQL(app)
 
-# Rota principal da aplicação
+def get_cursor():
+    """Cria e retorna um cursor para executar comandos SQL no banco de dados."""
+    return mysql.connection.cursor()
+
 @app.route('/')
 def home():
-    cur = mysql.connection.cursor()  # Cria um cursor para executar comandos SQL
-    # Executa uma consulta para selecionar tarefas que não estão marcadas como 'del'
-    cur.execute("SELECT *, DATE_FORMAT(task_date, '%d/%m/%Y às %H:%i') AS datebr FROM tasks WHERE task_status != 'del' ORDER BY task_status='concluido'")
+    """Rota principal que exibe as tarefas não deletadas."""
+    cur = get_cursor()
+    # Consulta para selecionar tarefas que não estão marcadas como 'del'
+    cur.execute("""
+        SELECT *, DATE_FORMAT(task_date, '%d/%m/%Y às %H:%i') AS datebr 
+        FROM tasks 
+        WHERE task_status != 'del' 
+        ORDER BY task_status='concluido'
+    """)
     tasks = cur.fetchall()  # Obtém todos os resultados da consulta
-    # print('\n\n', tasks, '\n\n')  # Para depuração: imprime as tarefas no console
     cur.close()  # Fecha o cursor
     return render_template('.home.html', tasks=tasks)  # Renderiza o template com as tarefas
 
-# Rota para adicionar uma nova tarefa
 @app.route('/add', methods=['GET', 'POST'])
 def add_task():
+    """Rota para adicionar uma nova tarefa."""
     if request.method == 'POST':  # Verifica se o método da requisição é POST
         title = request.form['title']  # Obtém o título da tarefa do formulário
         description = request.form['description']  # Obtém a descrição da tarefa do formulário
-        cur = mysql.connection.cursor()  # Cria um cursor
-        # Executa uma inserção no banco de dados
-        cur.execute(
-            "INSERT INTO tasks (task_title, task_description) VALUES (%s, %s)", (title, description))
+        cur = get_cursor()  # Cria um cursor
+        # Insere a nova tarefa no banco de dados
+        cur.execute("INSERT INTO tasks (task_title, task_description) VALUES (%s, %s)", (title, description))
         mysql.connection.commit()  # Confirma a transação
         cur.close()  # Fecha o cursor
         return redirect(url_for('home'))  # Redireciona para a página inicial
     return render_template('add_task.html')  # Renderiza o template para adicionar uma tarefa
 
-# Rota para editar uma tarefa existente
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_task(id):
-    cur = mysql.connection.cursor()  # Cria um cursor
+    """Rota para editar uma tarefa existente."""
+    cur = get_cursor()  # Cria um cursor
     if request.method == 'POST':  # Verifica se o método da requisição é POST
-        title = request.form['title']  # Obtém o título do formulário
-        description = request.form['description']  # Obtém a descrição do formulário
-        # Verifica se o checkbox de status está marcado
-        if request.form.get('status') == 'on':
-            status = 'concluído'  # Define como 'concluído' se marcado
-        else:
-            status = 'pendente'  # Define como 'pendente' se não
+        title = request.form['title']  # Obtém o título da tarefa do formulário
+        description = request.form['description']  # Obtém a descrição da tarefa do formulário
+        status = 'concluído' if request.form.get('status') == 'on' else 'pendente'  # Define o status da tarefa
+        current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Obtém a data e hora atuais
 
-        # print('\n\n\n', status, '\n\n\n')  # Para depuração: imprime o status no console
-
-        # Atualiza a tarefa, incluindo a data de criação
-        current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        # Executa uma atualização na tarefa especificada pelo ID
-        cur.execute("UPDATE tasks SET task_title=%s, task_description=%s, task_date=%s, task_status=%s WHERE task_id=%s",
-                    (title, description, current_datetime, status, id,))
+        # Atualiza a tarefa no banco de dados
+        cur.execute("""
+            UPDATE tasks 
+            SET task_title=%s, task_description=%s, task_date=%s, task_status=%s 
+            WHERE task_id=%s
+        """, (title, description, current_datetime, status, id))
         mysql.connection.commit()  # Confirma a transação
         cur.close()  # Fecha o cursor
         return redirect(url_for('home'))  # Redireciona para a página inicial
-    # Se não for uma requisição POST, busca a tarefa para editar
+
+    # Obtém os dados da tarefa a ser editada
     cur.execute("SELECT * FROM tasks WHERE task_id=%s", (id,))
-    task = cur.fetchone()  # Obtém a tarefa específica
-
-    # print("\n\n", task, "\n\n")  # Para depuração: imprime a tarefa no console
-
-    # Verifica se a tarefa está marcada como 'concluído' para o checkbox
-    if task[4] == 'concluido':
-        checked = 'checked'  # Marca o checkbox como selecionado
-    else:
-        checked = ''  # Não marca o checkbox
-
+    task = cur.fetchone()  # Obtém a tarefa
+    checked = 'checked' if task[4] == 'concluido' else ''  # Marca como concluída se aplicável
     cur.close()  # Fecha o cursor
-    return render_template('edit_task.html', task=task, checked=checked, action='upd')  # Renderiza o template de edição
+    return render_template('edit_task.html', task=task, checked=checked)  # Renderiza o template de edição
 
-# Rota para deletar uma tarefa
 @app.route('/delete/<int:id>')
 def delete_task(id):
-    cur = mysql.connection.cursor()  # Cria um cursor
-    # Executa uma atualização para marcar a tarefa como 'del'
-    cur.execute("UPDATE tasks SET task_status = 'del' WHERE task_id=%s", (id,))
+    """Rota para deletar uma tarefa, marcando-a como 'del'."""
+    cur = get_cursor()  # Cria um cursor
+    cur.execute("UPDATE tasks SET task_status = 'del' WHERE task_id=%s", (id,))  # Marca a tarefa como deletada
     mysql.connection.commit()  # Confirma a transação
     cur.close()  # Fecha o cursor
-    return redirect(url_for('home', action='del'))  # Redireciona para a página inicial
+    return redirect(url_for('home'))  # Redireciona para a página inicial
 
-# Manipulador de erro 404
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404  # Renderiza a página de erro 404
+    """Rota para tratar erros 404 (página não encontrada)."""
+    return render_template('404.html'), 404  # Renderiza uma página de erro 404
 
-# Verifica se o script está sendo executado diretamente
 if __name__ == '__main__':
     app.run(debug=True)  # Inicia a aplicação em modo de depuração
